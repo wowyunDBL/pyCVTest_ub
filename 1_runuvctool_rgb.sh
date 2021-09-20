fa7d19fb6e949f999a4846d855a3d66988f45bb6
Chiayun Chuang <wowyunpg@gmail.com>
2021-09-20 18:19:56 +0800

    [New] rgb.py

---
+++ 1_runuvctool_rgb.sh
@@ -0,0 +1,116 @@
+#!/bin/sh
+
+adb devices
+adb kill-server
+adb start-server
+adb wait-for-device
+
+# Check boot ready and enter main screen.
+status=0
+for cnt in $(seq 1 20)
+do
+    status=0
+    rm "check.txt"
+    sleep 2s
+    adb shell getprop dev.bootcomplete > check.txt
+    filename='check.txt'
+    while IFS='' read -r line || [ -n "$line" ]; do
+        #echo $cnt
+        #echo $line
+        if [ $line -eq '1' ]; then
+            #echo "pass"
+            status=1
+        fi
+    done < $filename
+    if [ $status -eq '1' ]; then
+        break;
+    fi
+    echo 'Wait to open camera...'
+    sleep 3s
+done
+if [ $status -eq '0' ]; then
+    echo 'Open camera failed...'
+    exit 1
+fi
+#read -p "Press 'Enter' to continue... " var_name
+
+# Open alcamera app.
+adb root
+adb wait-for-device
+adb remount
+adb shell setenforce 0
+
+# Clean all app
+adb shell input keyevent 3
+adb shell input keyevent KEYCODE_APP_SWITCH
+adb shell input tap 925 180
+sleep 0.5s
+
+adb shell input keyevent 4
+adb shell input keyevent 4
+adb shell input keyevent 4
+adb shell am start com.example.land.altek.alcamera/com.example.land.altek.alcamera.MainActivity
+sleep 2
+adb shell input tap 1784 758
+sleep 0.3s
+adb shell input tap 1402 350
+sleep 0.3s
+adb shell input tap 756 302
+sleep 0.3s
+adb shell input tap 756 712
+sleep 1s
+adb shell input tap 1480 202
+sleep 1s
+adb shell input tap 1214 892
+sleep 5s
+
+# Reset frame.en flag.
+adb shell setprop persist.al.frame.en 0
+
+# Check distance preview streaming on.
+for cnt in $(seq 1 12)
+do
+    status=0
+    #rm "camstatus.txt"
+    echo 'Check distance preview on...'
+    sleep 1s
+    adb shell getprop persist.al.frame.en > camstatus.txt  #echo '1' > check.txt
+    filename='camstatus.txt'
+    while IFS='' read -r line || [ -n "$line" ]; do
+        #echo $cnt
+        #echo $line
+        if [ $line -eq '1' ]; then
+            #echo "pass"
+            status=1
+        fi
+    done < $filename
+    if [ $status -eq '1' ]; then
+        break;
+    fi
+done
+if [ $status -eq '0' ]; then
+    echo 'Camera preview failed...'
+    exit 1
+fi
+#read -p "Press 'Enter' to continue... " var_name
+
+# Open UVC PC tool
+clear
+#echo UVC device FW version:
+#adb shell "cat /data/firmware/fw_version"
+echo ""
+echo "start..."
+sleep 2
+python3 pyCVTest5p_rgb.py
+# Get return code (wait to implement)
+echo ""
+
+# Close app
+echo 'Close app'
+sh ./3_CloseApp.sh
+
+# pause (if need)
+read -p "Press 'Enter' to continue... " var_name
+
+exit 0
+
---
+++ pyCVTest5p_rgb.py
@@ -0,0 +1,169 @@
+
+import cv2
+import numpy as np
+import time
+
+gtestmethod = 2
+gDistInMin = 200
+gDistInMax = 5000
+dev = 200 #202
+cnt = 10
+g_SaveDist = 0
+g_wait_ms = 50
+g_wait_1s = 1000
+DepthLUT_table = [0]*1024
+ColorLUT_table = [0]*1024*3
+g_rgb=1
+
+def ShowVersions():
+    print('cv2 ver=%s' % cv2.__version__)
+    #print('PIL ver=%s' % Image.__version__)
+    print('numpy ver=%s' % np.__version__)
+
+def opencv_cfg():
+    global g_rgb
+    cap.set(cv2.CAP_PROP_FORMAT, -1.0)
+    #cap.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc('M','J','P','G'))
+    cap.set(cv2.CAP_PROP_CONVERT_RGB, 0.0)
+    cap.set(cv2.CAP_PROP_MODE, 0.0)
+    if g_rgb == 1:
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640.0)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 630.0)
+    else:
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640.0)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 360.0)
+
+def get_opencv_cfg():
+    vcformat = cap.get(cv2.CAP_PROP_FORMAT)
+    fourcc = cap.get(cv2.CAP_PROP_FOURCC)
+    ConvertRGB = cap.get(cv2.CAP_PROP_CONVERT_RGB)
+    Mode = cap.get(cv2.CAP_PROP_MODE)
+    W = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    H = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    print("Get cv2 property:")
+    print("vcformat = %f" % vcformat)
+    print("fourcc = %f" % fourcc)
+    print("ConvertRGB = %f" % ConvertRGB)
+    print("Mode = %f" % Mode)
+    print("W = %f, H = %f\n" % (W, H))
+
+def LoadDepthLUT_Table():
+    i=0
+    rf = open('./lut/depth_lut.txt', 'r')
+    for line in rf.readlines():
+        DepthLUT_table[i] = line
+        i = i+1
+    rf.close()
+
+def LoadColorLUT_Table():
+    i=0
+    rf = open('./lut/color_lut.txt', 'r')
+    for line in rf.readlines():
+        S0, S1, S2, S3 = line.split(',', 4)
+        ColorLUT_table[i] = int(S0, 16)
+        ColorLUT_table[i+1] = int(S1, 16)
+        ColorLUT_table[i+2] = int(S2, 16)
+        i = i+3
+    rf.close()
+    
+def PixelCvt_Dist2RGBnp(Inframe, width, height, pRGB, InMin, InMax, mapping):
+    depthlut = np.zeros((width,height), np.uint16)
+    depthlut = 1023 - mapping[Inframe - InMin]
+    ftmp=np.reshape(pRGB, (width,height,3))
+    
+    ftmp[:,:,0]= ColorLUT_table_np[depthlut[:,:]*3+2] # B
+    ftmp[:,:,1]= ColorLUT_table_np[depthlut[:,:]*3+1] # G
+    ftmp[:,:,2]= ColorLUT_table_np[depthlut[:,:]*3]   # R
+    
+    ftmp[Inframe > InMax, 0] = 0xFF # B
+    ftmp[Inframe > InMax, 1] = 0 # G
+    ftmp[Inframe > InMax, 2] = 0 # R
+    
+    
+# show versions
+ShowVersions()
+
+# select camera, 0, or 1, or ...
+cap = cv2.VideoCapture(dev, cv2.CAP_ANY)
+
+# Check camera open status
+while not cap.isOpened():
+    cv2.waitKey(g_wait_1s)
+    cap = cv2.VideoCapture(dev, cv2.CAP_ANY)
+    print("Wait for opening camera...")
+    if cnt <= 0:
+        break
+
+if cap.isOpened():
+    cnt = 0
+    opencv_cfg()
+    w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    print("1 w=%d, h=%d" % (w, h))
+
+    #LoadDepthLUT_Table()
+    LoadColorLUT_Table()
+    get_opencv_cfg()
+    
+    table_size = gDistInMax - gDistInMin + 1
+    mappinglist = [0]*65535 #table_size
+    for i in range(0, table_size):
+        mappinglist[i] = round(i * 1023 / (gDistInMax - gDistInMin))
+    # converting list to array
+    mapping = np.array(mappinglist)
+    ColorLUT_table_np = np.array(ColorLUT_table)
+    #tic1 = tic2 = 0
+    
+    while(True):
+        #tic=time.time()
+        # get 1 image from camera
+        ret, frame_cv = cap.read()
+        if ret == 0 :
+            print("No video frame")
+            ch = cv2.waitKey(g_wait_1s)
+            continue
+        #if gtestmethod == 4 :
+        if g_rgb == 1:
+            imgSize = (h*2,w,1)
+            frame_np = np.frombuffer(frame_cv, dtype=np.uint8).reshape(imgSize)
+            h_rgb=540
+            ftmp = frame_np[:h_rgb,:w]
+            fBGR = cv2.cvtColor(ftmp,cv2.COLOR_YUV2BGR_NV21)
+            cv2.namedWindow('RGB', 1)
+            cv2.imshow('RGB', fBGR)
+        else:
+            imgSize = (w,h)
+            frame_np = np.frombuffer(frame_cv, dtype=np.uint16).reshape(imgSize)
+            fBGR = np.zeros((h,w,3), np.uint8)
+            PixelCvt_Dist2RGBnp(frame_np, w, h, fBGR, gDistInMin, gDistInMax, mapping)
+            cv2.namedWindow('Distance', 1)
+            cv2.imshow('Distance', fBGR)
+
+        # exit while loop when press q
+        ch = cv2.waitKey(g_wait_ms)
+        #if ch& 0xFF == ord('q'):
+        if ch& 0xFF == 27: #ESC key
+            break
+        elif ch& 0xFF == ord('s'):
+            g_SaveDist = 1;
+
+        if(g_SaveDist == 1):
+            if g_rgb == 1:
+                fcv = open('./Image/frame_rgb.raw','wb')
+                fcv.write(ftmp)
+            else:
+                fcv = open('./Image/frame_cv.raw','wb')
+                fcv.write(frame_cv)
+            fcv.close()
+            print("Write frame_cv.raw")
+            g_SaveDist = 0
+        cnt=cnt+1
+
+else:
+    print("Open camera fail!")
+
+# Release camera
+cap.release()
+
+# Close all OpenCV windows
+cv2.destroyAllWindows()
